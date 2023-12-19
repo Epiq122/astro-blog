3999ae30de8f0b74a235be1842030442c9ca65cb
Robert Gleason <l2ob122@gmail.com>
2023-12-19 10:38:41 -0800

    added some constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const SITE_DESCRIPTION =
+  'Articles, stories and tutorials from Tech People';
+export const HOMEPAGE_ARTICLES_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
